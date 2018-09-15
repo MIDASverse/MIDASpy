cace859dc3fd0035d0fb9d1cd6495315ab3abe3c
Oracen <alex.stenlake@gmail.com>
2018-09-15 18:52:59 +1000

    test

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     url=URL,
     # packages=find_packages(exclude=('tests',)),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['midas'],
+    py_modules=['imputer'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
