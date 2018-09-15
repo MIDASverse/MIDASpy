49bacf9a8779eca0c7646313c1207b2cb71a5b6b
Oracen <alex.stenlake@gmail.com>
2018-09-15 17:59:13 +1000

    Testing

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ URL = 'https://github.com/Oracen/MIDAS'
 EMAIL = 'alex.stenlake@gmail.com'
 AUTHOR = 'Alex Stenlake'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2
+VERSION = '0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
