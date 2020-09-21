cec4d99146a04dd6e4ee9d7feec454db58cbeff1
tsrobinson <thomas.robinson@politics.ox.ac.uk>
2020-09-21 17:00:31 +0100

    Add missing files for packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup, find_packages
 
 setup(name='midas',
-      version= '1.0',
+	  packages=['midas'],
+      version= '1.1',
+      licence= 'Apache',
       description= 'Implementation of Multiple Imputation using Denoising Autoencoders',
       url= 'http://github.com/ranjitlall/MIDAS',
       author= 'Ranjit Lall, Alex Stenlake, and Thomas Robinson',
       author_email= 'R.Lall@lse.ac.uk',
-      licence= 'Apache',
-      #packages= find_packages(),
-      packages=['midas'],
-      zip_safe=False)
+      download_url = 'https://github.com/MIDASverse/pyMIDAS/archive/v_1.1.tar.gz',
+      keywords = ['multiple imputation','neural networks','tensorflow'],
+      install_requires=[
+      	'tensorflow',
+      	'numpy',
+      	'matplotlib',
+      	'sklearn',
+      	'pandas',
+      	'random',
+         ],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: Apache Software License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+      )
